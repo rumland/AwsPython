e7ab62033338ed28a1d95b541fe09d0995ce4a9a
Robert Umland <robert.umland@cpsagu.com>
2018-03-02 14:25:48 -0700

    Python Lambda implementation from https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html'

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("hello")
+def my_handler(event, context):
+    message = 'Hello {} {}!'.format(event['first_name'], 
+                                    event['last_name'])  
+    return { 
+        'message' : message
+    }
